d65a5be636803333675782ad2fca14c4d1e76ab0
janicec3535 <janicec3535@gmail.com>
2021-10-08 10:15:58 -0700

    Phase 1: adjusted MathQuestion operation methods

--- src/main/ui/MathQuestion.java
+++ src/main/ui/MathQuestion.java
@@ -30,7 +30,7 @@ public class MathQuestion {
         String playerAnswer;  // force entry into loop
 
         System.out.println("Let's play Mad Math! Enter your answer in numbers!");
-        System.out.println(question());
+        question();
         System.out.println("Enter your answer:");
         playerAnswer = input.nextLine();
         int attempt = Integer.parseInt(playerAnswer);
@@ -38,16 +38,16 @@ public class MathQuestion {
     }
 
     // EFFECTS: generates random operation and makes a math question based on the operation
-    public String question() {
+    public void question() {
         Random rand = new Random();
         String operation = list.get(rand.nextInt(list.size()));
 
         if (operation.equals("Addition")) {
-            return addition();
+            addition();
         } else if (operation.equals("Subtraction")) {
-            return subtraction();
+            subtraction();
         } else {
-            return multiplication();
+            multiplication();
         }
     }
 
@@ -75,28 +75,28 @@ public class MathQuestion {
 
     // EFFECTS: chooses two random numbers between 1 and 100, and the mathAnswer will be the sum.
     //          Returns the addition question of the numbers and operator as a string
-    public String addition() {
+    public void addition() {
         int number1 = (int)(Math.random() * 100) + 1;
         int number2 = (int)(Math.random() * 100) + 1;
         mathAnswer = number1 + number2;
-        return "\tSolve: " + number1 + " + " + number2;
+        System.out.println("\tSolve: " + number1 + " + " + number2);
     }
 
     // EFFECTS: chooses two random numbers between 1 and 100, and the mathAnswer will be the difference.
     //          Returns the subtraction question of the numbers and operator as a string
-    public String subtraction() {
+    public void subtraction() {
         int number1 = (int)(Math.random() * 100) + 1;
         int number2 = (int)(Math.random() * 100) + 1;
         mathAnswer = number1 - number2;
-        return "\tSolve: " + number1 + " - " + number2;
+        System.out.println("\tSolve: " + number1 + " - " + number2);
     }
 
     // EFFECTS: chooses two random numbers between 1 and 10, and the mathAnswer will be the product.
     //          Returns the multiplication question of the numbers and operator as a string
-    public String multiplication() {
+    public void multiplication() {
         int number1 = (int)(Math.random() * 10) + 1;
         int number2 = (int)(Math.random() * 10) + 1;
         mathAnswer = number1 * number2;
-        return "\tSolve: " + number1 + " * " + number2;
+        System.out.println("\tSolve: " + number1 + " * " + number2);
     }
 }
