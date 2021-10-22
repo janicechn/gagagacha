ddfe4c37bb5df7c2164d4c8e39d7e095206b9845
janicec3535 <janicec3535@gmail.com>
2021-10-21 22:51:39 -0700

    Phase 2: completed reader test

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,8 +1,11 @@
 package persistence;
 
 import model.Player;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,10 +25,25 @@ public class JsonReaderTest {
 
     @Test
     void testReaderPlayer() {
+        Player samplePlayer = new Player("sample name", 10);
+        samplePlayer.addNotebook("message");
+        samplePlayer.addNotebook("fortune");
+        samplePlayer.removeBalance(7);
+        samplePlayer.addBalance(1);
+        //https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
+        JsonWriter writer = new JsonWriter("./data/testReaderPlayer.json");
+        try {
+            writer.open();
+            writer.write(samplePlayer);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            fail("Couldn't find file");
+        }
+
         JsonReader reader = new JsonReader("./data/testReaderPlayer.json");
         try {
             Player player = reader.read();
-            assertEquals("name", player.getName());
+            assertEquals("sample name", player.getName());
             assertEquals(2, player.getNotebook().size());
             assertEquals("message", player.getNotebook().get(0));
             assertEquals("fortune", player.getNotebook().get(1));
