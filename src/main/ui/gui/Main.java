d43fdddb74718e291cf11abd8c36699a29e247d9
janicec3535 <janicec3535@gmail.com>
2021-11-02 13:07:51 -0700

    Phase 3: added dialog in the start to ask for player name to make new player out of it

--- src/main/ui/gui/Main.java
+++ src/main/ui/gui/Main.java
@@ -1,5 +1,7 @@
 package ui.gui;
 
+import model.Player;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.logging.Level;
--- src/main/ui/gui/StartScreen.java
+++ src/main/ui/gui/StartScreen.java
@@ -1,5 +1,7 @@
 package ui.gui;
 
+import model.Player;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -11,16 +13,38 @@ public class StartScreen {
     // https://stackoverflow.com/questions/31526190/jframe-with-background-image-and-a-jpanel?noredirect=1&lq=1
     //https://stackoverflow.com/questions/22162398/how-to-set-a-background-picture-in-jpanel
 
+    Player player;
+
     public StartScreen() {
         Panel background = new Panel();
 
         JFrame frame = new JFrame("GAGAGACHA");
         frame.setContentPane(background);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Gui());
         frame.pack();
+        frame.add(new Gui());
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            // https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
+            String s = (String)JOptionPane.showInputDialog(frame,  "Enter your name:\n",
+                    "Create new player", JOptionPane.PLAIN_MESSAGE, null, null, "");
+
+            //If a string was returned, say so.
+            //https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
+            if ((s != null) && (s.length() > 0)) {
+                keepGoing = false;
+                player = new Player(s, 10);
+            }
+            else {
+                JOptionPane.showMessageDialog(frame,
+                        "Oops! Error in entering your player name. Unable to launch game.",
+                        "Input error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     public static class Panel extends JPanel {
